4e9a9f0ba903313e34cfb0784e4369c86e601325
Odija <user@Odijas-MacBook-Pro.local>
2026-02-05 08:38:58 +0100

    refine literally everything

--- src/App.jsx
+++ src/App.jsx
@@ -76,7 +76,7 @@ function App() {
 
   return (
     <ReactLenis root options={{ restoreScroll: true, lerp: 0.1, duration: 1.5 }}>
-      {/* 7. This wrapper hides the 'glitch' by keeping opacity at 0 until the jump is done */}
+      {/* 7. Hides the 'glitch' by keeping opacity at 0 until the jump is done */}
       <div 
         className={`transition-opacity duration-500 ease-in-out ${isLoaded ? "opacity-100" : "opacity-0"}`}
         style={{ backgroundColor: 'black' }} // Matches your site theme to hide white flashes
