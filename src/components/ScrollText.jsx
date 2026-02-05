079c071793aac9f03f2c6490de43e905a86506fc
Odija <user@Odijas-MacBook-Pro.local>
2026-02-05 08:52:09 +0100

    rewrite name mismatch

--- src/components/ScrollText.jsx
+++ src/components/ScrollText.jsx
@@ -4,7 +4,7 @@ import { useGSAP } from "@gsap/react";
 import { motion } from "framer-motion";
 import { LeftWreath, RightWreath } from "../assets/icons/Wreaths";
 import { brands } from "../data/brandLogos";
-import BibleVerses from "../data/bibleVerses";
+import { bibleVerses } from "../data/bibleVerses";
 
 // Animations
 const containerVariants = {
@@ -106,7 +106,7 @@ const TextReveal = ({ verse }) => {
 
 export default function ScrollText() {
   const selectedVerse = useMemo(() => {
-    return BibleVerses[Math.floor(Math.random() * BibleVerses.length)];
+    return bibleVerses[Math.floor(Math.random() * bibleVerses.length)];
   }, []);
 
   const logosBlock = [...brands, ...brands];
